772c997fe38b30f9322199cc0baf16697907350e
mmoedinhas <mmoedinhas@hotmail.com>
2018-10-19 16:32:51 +0100

    before i delete again

---
+++ src/StartApp.java
@@ -0,0 +1,13 @@
+import jade.core.Runtime;
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.wrapper.*; 
+
+public class StartApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

fa37aad9af7342fcb9f59dbfda7957abb5ff65ee
mmoedinhas <mmoedinhas@hotmail.com>
2018-10-26 16:53:43 +0100

    ISTO NAO E LDSO POR ISSO NAO PRECISO DE METER COMMITS DE JEITO >:)

--- src/AgentSuperPC.java
+++ src/AgentSuperPC.java
@@ -1,14 +1,16 @@
 import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
 
 public class AgentSuperPC extends Agent {
-	
-	private int memory; //PC's memory in kB
-	private int cpu; //PC's cpu in MHz
-	private int memoryTaken; //PC's memory taken by programs
-	private int cpuTaken; //PC's cpu taken by programs
-	
+
+	private int memory; // PC's memory in kB
+	private int cpu; // PC's cpu in MHz
+	private int memoryTaken; // PC's memory taken by programs
+	private int cpuTaken; // PC's cpu taken by programs
+
 	public void setup() {
-		System.out.println("Hello world! I'm Super PC!");
+		addBehaviour(new ListeningBehaviour());
 	}
 
 	public int getMemory() {
@@ -34,13 +36,29 @@ public class AgentSuperPC extends Agent {
 	public void setCpuTaken(int cpuTaken) {
 		this.cpuTaken = cpuTaken;
 	}
-	
+
 	public double getPercentageMemoryTaken() {
-		return ((double)memoryTaken/memory)*100;
+		return ((double) memoryTaken / memory) * 100;
 	}
-	
+
 	public double getPercentageCpuTaken() {
-		return ((double)cpuTaken/cpu)*100;
+		return ((double) cpuTaken / cpu) * 100;
 	}
-	
+
+	class ListeningBehaviour extends CyclicBehaviour {
+
+		public void action() {
+			ACLMessage msg = receive();
+			if (msg != null) {
+				System.out.println(msg);
+				ACLMessage reply = msg.createReply();
+				reply.setPerformative(ACLMessage.INFORM);
+				reply.setContent("Got your message!");
+				send(reply);
+			} else {
+				block();
+			}
+		}
+	}
+
 }

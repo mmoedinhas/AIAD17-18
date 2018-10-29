92da718d2bad0b2b20182fdfb2adfa1424ea05bb
Catarina Ferreira <up201506671@fe.up.pt>
2018-10-29 16:36:47 +0000

    superPC calculates if it can accept proposal or not (not final form)

--- src/agent/AgentSuperPC.java
+++ src/agent/AgentSuperPC.java
@@ -1,4 +1,8 @@
 package agent;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
@@ -37,9 +41,10 @@ public class AgentSuperPC extends Agent {
 	}
 	
 	class AnswerRequest extends ContractNetResponder {
-		
-		public AnswerRequest(Agent a, MessageTemplate mt) {
+		AgentSuperPC agent;
+		public AnswerRequest(AgentSuperPC a, MessageTemplate mt) {
 			super(a, mt);
+			this.agent = a;
 		}
 		
 		protected ACLMessage handleCfp(ACLMessage cfp) {
@@ -48,8 +53,39 @@ public class AgentSuperPC extends Agent {
 			//reply.setContent("I will do it for free!!!");
 			// ...
 			System.out.println("content");
-			System.out.println(cfp.getContent());
+			JSONParser parser = new JSONParser();
+			JSONObject content;
+			try {
+				content = (JSONObject) parser.parse(cfp.getContent());
+				int memoryNeeded = ((Long)content.get("memoryNeeded")).intValue();
+				int cpuNeeded = ((Long)content.get("cpuNeeded")).intValue();
+				int timeNeeded = ((Long)content.get("timeNeeded")).intValue();
+				if(canAccept(memoryNeeded,cpuNeeded)){
+					System.out.println("aceitei");
+				}else{
+					System.out.println("nao aceitei");
+				}
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+			
 			return reply;
+		}
+		
+		/**
+		 * Checks if we can accept the proposal
+		 * @param memoryNeeded
+		 * @param cpuNeeded
+		 * @return true if superPC has enough memory and cpu available
+		 */
+		protected boolean canAccept(int memoryNeeded, int cpuNeeded){
+			if(agent.getMemoryAvailable() > memoryNeeded && agent.getCpuAvailable() > cpuNeeded){
+				return true;
+			}
+			return false;
 		}
 		
 		
@@ -59,9 +95,17 @@ public class AgentSuperPC extends Agent {
 	public int getMemory() {
 		return memory;
 	}
+	
+	public int getMemoryAvailable() {
+		return memory - memoryTaken;
+	}
 
 	public int getCpu() {
 		return cpu;
+	}
+	
+	public int getCpuAvailable() {
+		return cpu - cpuTaken;
 	}
 
 	public int getMemoryTaken() {
